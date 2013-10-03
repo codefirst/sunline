6c8f9d0620d55310676eb96b88e98255743ada64
suer <suetsugu.r@gmail.com>
2013-10-03 19:14:47 +0900

    Changed: top page

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Sunline::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
+  root 'scripts#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
