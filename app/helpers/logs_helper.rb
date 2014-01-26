6bddfb18ce2a35f268e6b55fd5d423f03a4df0ad
suer <suetsugu.r@gmail.com>
2014-01-26 21:14:33 +0900

    move logic to controller

--- app/helpers/logs_helper.rb
+++ app/helpers/logs_helper.rb
@@ -1,13 +1,2 @@
 module LogsHelper
-  def post_hooks(log)
-    json = log.hook_json(log_url(log))
-
-    log.script.hooks.each do |hook|
-      begin
-        Faraday.post hook.url, payload: json
-      rescue => e
-        logger.error e
-      end
-    end
-  end
 end
