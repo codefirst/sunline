f9885f2c82ae2bcd69480d5cf8c08bebf3563e6f
suer <suetsugu.r@gmail.com>
2014-05-18 01:59:43 +0900

    Fixed: test failure

--- spec/controllers/hosts_controller_spec.rb
+++ spec/controllers/hosts_controller_spec.rb
@@ -2,6 +2,12 @@ require 'spec_helper'
 
 describe HostsController do
 
+  before do
+    user = User.new(name: 'name', nickname: 'nickname')
+    user.save
+    sign_in user
+  end
+
   describe "GET 'index'" do
     it "returns http success" do
       get 'index'
