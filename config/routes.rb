c8f86c8ce19847e341ab2fb1dcc46b4df73c5394
Ryo SUETSUGU <suetsugu.r@gmail.com>
2014-05-31 23:19:24 +0900

    Changed: sign-out by delete

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ Sunline::Application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'authentication' }
   devise_scope :user do
     get 'sign_in', to: 'authentication#login', as: :new_user_session
-    post 'sign_out', to: 'authentication#logout', as: :destroy_user_session
+    delete 'sign_out', to: 'authentication#logout', as: :destroy_user_session
   end
   get ':controller/:action', controller: 'authentication'
 
