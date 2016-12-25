36757fefb1f7df5163c87c744ce82af1544d981a
mallowlabs <mallowlabs@gmail.com>
2016-12-25 13:16:03 +0900

    Fixed: could not obtain a database connection within 5.000 seconds

--- app/models/concerns/slack_notify.rb
+++ app/models/concerns/slack_notify.rb
@@ -9,41 +9,43 @@ module SlackNotify
   def notify
     unless ENV['SLACK_WEBHOOK_URL'].blank?
       Thread.start do
-        lines = []
-        lines << "Sunline registered a log. Last 3 lines are:"
-        lines << '```'
-        lines << self.result.split("\n").last(3)
-        lines << '```'
+        ActiveRecord::Base.connection_pool.with_connection do
+          lines = []
+          lines << "Sunline registered a log. Last 3 lines are:"
+          lines << '```'
+          lines << self.result.split("\n").last(3)
+          lines << '```'
 
-        begin
-          notifier = Slack::Notifier.new ENV['SLACK_WEBHOOK_URL']
-          notifier.ping lines.join("\n"), attachments: [{
-            color: "#8daec2",
-            fields: [
-              {
-                title: "Script",
-                value: self.script.name,
-                short: true
-              },
-              {
-                title: "Uploaded",
-                value: self.formatted_created_at,
-                short: true
-              },
-              {
-                title: "Host",
-                value: self.host,
-                short: true
-              },
-              {
-                title: "Size",
-                value: number_to_human_size(self.result.bytesize),
-                short: true
-              }
-            ]
-          }]
-        rescue => e
-          Rails.logger.error e
+          begin
+            notifier = Slack::Notifier.new ENV['SLACK_WEBHOOK_URL']
+            notifier.ping lines.join("\n"), attachments: [{
+              color: "#8daec2",
+              fields: [
+                {
+                  title: "Script",
+                  value: self.script.name,
+                  short: true
+                },
+                {
+                  title: "Uploaded",
+                  value: self.formatted_created_at,
+                  short: true
+                },
+                {
+                  title: "Host",
+                  value: self.host,
+                  short: true
+                },
+                {
+                  title: "Size",
+                  value: number_to_human_size(self.result.bytesize),
+                  short: true
+                }
+              ]
+            }]
+          rescue => e
+            Rails.logger.error e
+          end
         end
       end
     end
