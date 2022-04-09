0c129b7b3c99c80a185e34ed334e3ed25b492827
mallowlabs <mallowlabs@gmail.com>
2022-04-09 21:44:28 +0900

    fix DEPRECATION WARNING: [Devise] including  is deprecated and will be removed from Devise.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -39,5 +39,5 @@ RSpec.configure do |config|
   #     --seed 1234
   config.order = "random"
 
-  config.include Devise::TestHelpers, :type => :controller
+  config.include Devise::Test::ControllerHelpers, :type => :controller
 end
