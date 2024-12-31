4184199d7d3bf5b5c3337abcd9dec1e6003bc701
mallowlabs <mallowlabs@gmail.com>
2024-12-31 13:52:31 +0900

    Remove bootstrap CDN

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,6 @@
 // Entry point for the build script in your package.json
 
+import "bootstrap/dist/js/bootstrap";
 import CodeMirror from "codemirror";"codemirror/lib/codemirror";
 import "codemirror/mode/shell/shell";
 import "@fortawesome/fontawesome-free/js/all";
