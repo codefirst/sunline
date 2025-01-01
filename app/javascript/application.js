7e13ef9b1d63a8e1ccce41bc788064859d7543c1
mallowlabs <mallowlabs@gmail.com>
2025-01-01 15:21:54 +0900

    Remove jQuery CDN

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,8 @@
 // Entry point for the build script in your package.json
 
+import jQuery from 'jquery';
+window.jQuery = window.$ = jQuery;
+
 import "bootstrap/dist/js/bootstrap";
 import CodeMirror from "codemirror";"codemirror/lib/codemirror";
 import "codemirror/mode/shell/shell";
