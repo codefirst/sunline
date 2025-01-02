b5f6d03f0f47ba1472b42daa23d41f8b26d1e1ba
mallowlabs <mallowlabs@gmail.com>
2025-01-02 13:49:26 +0900

    Update to CodeMirror 6

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -4,10 +4,35 @@ import jQuery from 'jquery';
 window.jQuery = window.$ = jQuery;
 
 import("bootstrap/dist/js/bootstrap"); // use () to import jQuery
-import CodeMirror from "codemirror";"codemirror/lib/codemirror";
-import "codemirror/mode/shell/shell";
+import { EditorView, lineNumbers, keymap } from "@codemirror/view";
+import { EditorState } from "@codemirror/state";
+import { history, defaultKeymap, historyKeymap } from '@codemirror/commands';
+import { StreamLanguage, syntaxHighlighting, defaultHighlightStyle } from "@codemirror/language";
+import { shell } from '@codemirror/legacy-modes/mode/shell';
 import "@fortawesome/fontawesome-free/js/all";
 
 import("./bootstrap-fileupload"); // use () to import $
 
-window.CodeMirror = CodeMirror;
+window.editorFromTextArea = function (textarea, options) {
+  let extensions = [
+    history(),
+    keymap.of(defaultKeymap),
+    keymap.of(historyKeymap),
+    StreamLanguage.define(shell),
+    syntaxHighlighting(defaultHighlightStyle),
+    lineNumbers()
+  ];
+  if (options.readOnly) {
+    extensions.push(EditorState.readOnly.of(true));
+  }
+
+  const view = new EditorView({doc: textarea.value, extensions});
+  textarea.parentNode.insertBefore(view.dom, textarea);
+  textarea.style.display = "none";
+  if (textarea.form) {
+    textarea.form.addEventListener("submit", () => {
+      textarea.value = view.state.doc.toString();
+    });
+  }
+  return view;
+}
