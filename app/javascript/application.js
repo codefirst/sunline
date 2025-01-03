ec82b15c1397f190aca4165c18b86b35e5a4afb3
mallowlabs <mallowlabs@gmail.com>
2025-01-03 09:29:10 +0900

    Refactor

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -4,41 +4,10 @@ import jQuery from 'jquery';
 window.jQuery = window.$ = jQuery;
 
 import("bootstrap/dist/js/bootstrap"); // use () to import jQuery
-import { EditorView, lineNumbers, keymap } from "@codemirror/view";
-import { EditorState } from "@codemirror/state";
-import { history, defaultKeymap, historyKeymap } from '@codemirror/commands';
-import { StreamLanguage, syntaxHighlighting, defaultHighlightStyle } from "@codemirror/language";
-import { shell } from '@codemirror/legacy-modes/mode/shell';
+
 import "@fortawesome/fontawesome-free/js/all";
 
 import("./bootstrap-fileupload"); // use () to import $
 
-window.editorFromTextArea = function (textarea, options) {
-  let extensions = [
-    history(),
-    keymap.of(defaultKeymap),
-    keymap.of(historyKeymap),
-    StreamLanguage.define(shell),
-    syntaxHighlighting(defaultHighlightStyle)
-  ];
-  if (options.firstLineNumber) {
-    extensions.push(lineNumbers(
-      { formatNumber: (n) => (n + options.firstLineNumber - 1).toString() }
-    ));
-  } else {
-    extensions.push(lineNumbers());
-  }
-  if (options.readOnly) {
-    extensions.push(EditorState.readOnly.of(true));
-  }
-
-  const view = new EditorView({doc: textarea.value, extensions});
-  textarea.parentNode.insertBefore(view.dom, textarea);
-  textarea.style.display = "none";
-  if (textarea.form) {
-    textarea.form.addEventListener("submit", () => {
-      textarea.value = view.state.doc.toString();
-    });
-  }
-  return view;
-}
+import { editorFromTextArea } from "./codemirror";
+window.editorFromTextArea = editorFromTextArea;
