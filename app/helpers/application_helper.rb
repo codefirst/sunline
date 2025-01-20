5d40db2ab13261d617a4cf70cb1346e78ba65acf
mallowlabs <mallowlabs@gmail.com>
2025-01-20 22:30:57 +0900

    Replace hash rocket syntax in app/helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -19,8 +19,8 @@ module ApplicationHelper
 
       Array(message).each do |msg|
         text = content_tag(:div,
-                            content_tag(:button, raw("&times;"), :class => "close", "data-dismiss" => "alert") +
-                            msg.html_safe, :class => "alert fade in alert-#{type}")
+                            content_tag(:button, raw("&times;"), class: "close", "data-dismiss": "alert") +
+                            msg.html_safe, class: "alert fade in alert-#{type}")
         flash_messages << text if msg
       end
     end
