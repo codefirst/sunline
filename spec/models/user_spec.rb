37e90a5879a2f8a96d8140895deb04004d066e80
mallowlabs <mallowlabs@gmail.com>
2026-03-24 20:57:51 +0900

    Add basic tests for User model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { User.new(name: 'test user', nickname: 'test') }
+
+  describe 'attributes' do
+    subject { user }
+    its(:name) { is_expected.to eq 'test user' }
+    its(:nickname) { is_expected.to eq 'test' }
+  end
+
+  describe 'validations' do
+    it 'is valid with name and nickname' do
+      expect(user).to be_valid
+    end
+  end
 end
